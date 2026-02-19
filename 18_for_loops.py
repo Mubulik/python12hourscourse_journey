82e4f8fb48a74625c8a2709cd672c8276c6e8520
Mubulik <lilian.lambert70@gmail.com>
2026-02-19 12:08:41 +0100

    Chapter 18 -> For loops

--- 18_for_loops.py
+++ 18_for_loops.py
@@ -1,2 +1,15 @@
 # for loops = éxecute un bloc de code un nombre de fois fixé
-#             Il est possible d'itérer sur une range, string, sequence etc...              
+#             Il est possible d'itérer sur une range, string, sequence etc...
+
+# card_number = "1234-5678-9012"
+# 
+# for x in card_number:
+#     print(x)
+
+for x in range(1, 21):
+    if x == 13:         # -> on a skip une itération
+        continue
+    if x == 14:
+        break           # -> on arrête la boucle à la condition
+    else:
+        print(x)
