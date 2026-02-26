05e2b625e770eef36cfca802e3e716d5bbf374b3
Mubulik <lilian.lambert70@gmail.com>
2026-02-26 15:42:53 +0100

    Chapter 39 : modules

---
+++ 39_modules/main.py
@@ -0,0 +1,11 @@
+# module = Fichier contenant du code que l'on souhaite include dans notre programme
+#          Utiliser "import" pour inclure un module (built-in ou le sien)
+#          Utile pour séparer un gros programme en plusieurs fichiers séparés
+
+# import math
+# import math as m
+
+import exemple
+
+print(exemple.square(2))
+
